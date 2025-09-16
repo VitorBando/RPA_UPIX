0c90a3571fe77182b25e9f2c122c55a4c9a372c1
VitorBando <vitorm.804@gmail.com>
2025-09-16 13:14:14 -0300

    condicionais pelos arquivos de a vencer e vencidos terem o mesmo nome na extração

--- Tratamento_Arquivos_Pos_Vendas/transform_accounts_due.py
+++ Tratamento_Arquivos_Pos_Vendas/transform_accounts_due.py
@@ -15,32 +15,61 @@ origem = os.path.join(r"C:\Downloads\Bases_Relatorio", hoje)
 arquivos = glob.glob(os.path.join(origem, '*servicos*.xlsx'))
 caminho_destino = os.path.join(destino, "base_a_vencer.xlsx")
 caminho_destino_csv = os.path.join(destino, "base_a_vencer.csv")
-arquivo = arquivos[0]
+arquivo1 = arquivos[0]
+arquivo2 = arquivos[1]
 
 # # Lendo arquivo Excel
-df = pd.read_excel(arquivo, dtype=str)
+df_view1 = pd.read_excel(arquivo1, dtype=str)
+df_view2 = pd.read_excel(arquivo2, dtype=str)
 
-# # Limpar e padronizar nomes das colunas
-df.columns = df.columns.map(lambda x: x.strip().lower().replace(" ", "_"))
+# condição para validar a vencer
+if len(df_view1) < len(df_view2):
+    # Limpar e padronizar nomes das colunas
+    df_view1.columns = df_view1.columns.map(lambda x: x.strip().lower().replace(" ", "_"))
 
-df_final = df[[ 'id_cliente_servico_contrato'
-                 ,'nome_razaosocial'
-                 ,'valor'
-                 ,'validade'
-                 ,'cidade'
-                 ,'servico_status'
-                 ,'email_principal'
-                 ,'data_inicio_contrato'
-                 ,'estado'
-                 ,'telefone_primario'
-                 ,'servico'
-                 ,'email_secundario'
-                 ,'data_fim_contrato'
-                ]]
+    df_final = df_view1[[ 'id_cliente_servico_contrato'
+                    ,'nome_razaosocial'
+                    ,'valor'
+                    ,'validade'
+                    ,'cidade'
+                    ,'servico_status'
+                    ,'email_principal'
+                    ,'data_inicio_contrato'
+                    ,'estado'
+                    ,'telefone_primario'
+                    ,'servico'
+                    ,'email_secundario'
+                    ,'data_fim_contrato'
+                    ]]
 
-# # Removendo linhas com '-' no id_cliente
-df_final = df_final[~df_final['id_cliente_servico_contrato'].str.contains("-", na=False)]
+    # Removendo linhas com '-' no id_cliente
+    df_final = df_final[~df_final['id_cliente_servico_contrato'].str.contains("-", na=False)]
 
-# Salvando arquivo para utilização de graficos ou update em db
-df_final.to_excel(caminho_destino, index=False, engine="openpyxl")
-df_final.to_csv(caminho_destino_csv, index=False, sep=';', encoding='utf-8')
+    # Salvando arquivo para utilização de graficos ou update em db
+    df_final.to_excel(caminho_destino, index=False, engine="openpyxl")
+    df_final.to_csv(caminho_destino_csv, index=False, sep=';', encoding='utf-8')
+else:
+    # Limpar e padronizar nomes das colunas
+    df_view2.columns = df_view2.columns.map(lambda x: x.strip().lower().replace(" ", "_"))
+
+    df_final = df_view2[[ 'id_cliente_servico_contrato'
+                    ,'nome_razaosocial'
+                    ,'valor'
+                    ,'validade'
+                    ,'cidade'
+                    ,'servico_status'
+                    ,'email_principal'
+                    ,'data_inicio_contrato'
+                    ,'estado'
+                    ,'telefone_primario'
+                    ,'servico'
+                    ,'email_secundario'
+                    ,'data_fim_contrato'
+                    ]]
+
+    # Removendo linhas com '-' no id_cliente
+    df_final = df_final[~df_final['id_cliente_servico_contrato'].str.contains("-", na=False)]
+
+    # Salvando arquivo para utilização de graficos ou update em db
+    df_final.to_excel(caminho_destino, index=False, engine="openpyxl")
+    df_final.to_csv(caminho_destino_csv, index=False, sep=';', encoding='utf-8')
--- Tratamento_Arquivos_Pos_Vendas/transform_overdue_accounts.py
+++ Tratamento_Arquivos_Pos_Vendas/transform_overdue_accounts.py
@@ -15,38 +15,73 @@ origem = os.path.join(r"C:\Downloads\Bases_Relatorio", hoje)
 arquivos = glob.glob(os.path.join(origem, '*servicos*.xlsx'))
 caminho_destino = os.path.join(destino, "base_vencidos.xlsx")
 caminho_destino_csv = os.path.join(destino, "base_vencidos.csv")
-arquivo = arquivos[1]
+arquivo1 = arquivos[0]
+arquivo2 = arquivos[1]
 
 # # Lendo arquivo Excel
-df = pd.read_excel(arquivo, dtype=str)
+df_view1 = pd.read_excel(arquivo1, dtype=str)
+df_view2 = pd.read_excel(arquivo2, dtype=str)
 
-# # Limpar e padronizar nomes das colunas
-df.columns = df.columns.map(lambda x: x.strip().lower().replace(" ", "_"))
+# condição para validar a vencer
+if len(df_view1) > len(df_view2):
+    # Limpar e padronizar nomes das colunas
+    df_view1.columns = df_view1.columns.map(lambda x: x.strip().lower().replace(" ", "_"))
 
 
-df_final = df[[ 'id_cliente'
-               ,'nome_razaosocial'
-               ,'servico_status'
-               ,'telefone_primario'
-               ,'telefone_secundario'
-               ,'servico'
-               ,'valor'
-               ,'email_principal'
-               ,'email_secundario'
-               ,'cpf_cnpj'
-               ,'data_venda'
-               ,'validade'
-               ,'data_inicio_contrato'
-               ,'data_fim_contrato'
-               ,'endereco'
-               ,'bairro'
-               ,'cidade'
-               ,'estado'
-                ]]
+    df_final = df_view1[[ 'id_cliente'
+                ,'nome_razaosocial'
+                ,'servico_status'
+                ,'telefone_primario'
+                ,'telefone_secundario'
+                ,'servico'
+                ,'valor'
+                ,'email_principal'
+                ,'email_secundario'
+                ,'cpf_cnpj'
+                ,'data_venda'
+                ,'validade'
+                ,'data_inicio_contrato'
+                ,'data_fim_contrato'
+                ,'endereco'
+                ,'bairro'
+                ,'cidade'
+                ,'estado'
+                    ]]
 
-# # Removendo linhas com '-' no id_cliente
-df_final = df_final[~df_final['id_cliente'].str.contains("-", na=False)]
+    # # Removendo linhas com '-' no id_cliente
+    df_final = df_final[~df_final['id_cliente'].str.contains("-", na=False)]
 
-# Salvando arquivo para utilização de graficos ou update em db
-df_final.to_excel(caminho_destino, index=False, engine="openpyxl")
-df_final.to_csv(caminho_destino_csv, index=False, sep=';', encoding='utf-8')
+    # Salvando arquivo para utilização de graficos ou update em db
+    df_final.to_excel(caminho_destino, index=False, engine="openpyxl")
+    df_final.to_csv(caminho_destino_csv, index=False, sep=';', encoding='utf-8')
+else:
+    # Limpar e padronizar nomes das colunas
+    df_view2.columns = df_view2.columns.map(lambda x: x.strip().lower().replace(" ", "_"))
+
+
+    df_final = df_view2[[ 'id_cliente'
+                ,'nome_razaosocial'
+                ,'servico_status'
+                ,'telefone_primario'
+                ,'telefone_secundario'
+                ,'servico'
+                ,'valor'
+                ,'email_principal'
+                ,'email_secundario'
+                ,'cpf_cnpj'
+                ,'data_venda'
+                ,'validade'
+                ,'data_inicio_contrato'
+                ,'data_fim_contrato'
+                ,'endereco'
+                ,'bairro'
+                ,'cidade'
+                ,'estado'
+                    ]]
+
+    # # Removendo linhas com '-' no id_cliente
+    df_final = df_final[~df_final['id_cliente'].str.contains("-", na=False)]
+
+    # Salvando arquivo para utilização de graficos ou update em db
+    df_final.to_excel(caminho_destino, index=False, engine="openpyxl")
+    df_final.to_csv(caminho_destino_csv, index=False, sep=';', encoding='utf-8')
